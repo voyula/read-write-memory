78787f4ec2fc07395dc88ec6c9730e75ccf09f12
voyula <40173603+voyula@users.noreply.github.com>
2018-09-29 10:56:15 +0300

    Create read-write-memory.cpp

---
+++ read-write-memory.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "Windows.h"
+
+int main()
+{
+	int newValue = 1000;
+	int read = 0;
+	HWND hwnd = FindWindowA(NULL, "Application Name");
+	if(hwnd == NULL) {
+		printf("Error");
+		Sleep(5000);
+		exit(-1);
+	}
+
+	DWORD pID;
+	GetWindowThreadProcessId(hwnd, &pID);
+	HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
+	if(handle == NULL) {
+		printf("Error");
+		Sleep(5000);
+		exit(-1);
+	}
+
+	ReadProcessMemory(handle, (PBYTE*) 0x00000000, &read, sizeof(int), 0);
+	WriteProcessMemory(handle, (LPVOID) 0x00000000, &newValue, sizeof(int), 0);
+	Sleep(10000);
+
+	return 0;
+}
+
