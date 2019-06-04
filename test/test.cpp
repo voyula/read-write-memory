9ab54235a38e854a555de390c00951a0577eede1
Musa <40173603+voyula@users.noreply.github.com>
2019-06-05 01:54:48 +0300

    Create test.cpp

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include "../read_write_memory.h"
+
+using namespace Memory;
+
+int main()
+{
+    read_write_memory instance;
+    int newValue = 1000;
+    int process = instance.getProcessID("Application Name");
+    printf("%d", instance.readMemory(process, 0x00000000));
+    printf("%d", instance.writeMemory(process, 0x00000000, 1000));
+    Sleep(10000);
+
+    return 0;
+}
